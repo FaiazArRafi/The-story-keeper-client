acc8542df759533f35fac4fb6b4364b6d4c479c7
Faiaz <faiazarrafi@gmail.com>
2022-11-26 22:27:22 +0600

    admin dashborad done

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthProvider';
+import useAdmin from '../Hooks/useAdmin';
 import Header from '../Pages/Shared/Header/Header';
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email)
+
+    // const { data: users = [], refetch } = useQuery({
+    //     queryKey: ['users'],
+    //     queryFn: async () => {
+    //         const res = await fetch('http://localhost:5000/users');
+    //         const data = await res.json();
+    //         return data;
+    //     }
+    // });
+
     return (
         <div>
             <Header></Header>
@@ -18,9 +33,16 @@ const DashboardLayout = () => {
                         <div className="flex-1 px-2 mx-2">Dashboard</div>
                         <div className="flex-none hidden lg:block">
                             <ul className="menu menu-horizontal">
-                                <li><Link to="/dashboard">My Orders</Link></li>
-                                <li><Link to="/dashboard/allsellers">All Sellers</Link></li>
-                                <li><Link to="/dashboard/allbuyers">All Buyers</Link></li>
+
+                                {
+                                    isAdmin ?
+                                        <>
+                                            <li><Link to="/dashboard/allsellers">All Sellers</Link></li>
+                                            <li><Link to="/dashboard/allbuyers">All Buyers</Link></li>
+                                        </>
+                                        :
+                                        <li><Link to="/dashboard">My Orders</Link></li>
+                                }
                             </ul>
                         </div>
                     </div>
